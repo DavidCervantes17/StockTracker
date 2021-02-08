84d1ee466b9c879671dd6252afc2e0682a0c89d4
DavidCervantes17 <41938035+DavidCervantes17@users.noreply.github.com>
2021-02-08 08:20:15 -0600

    Nueva pantalla coins screen

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,15 +1,44 @@
 import  React, {Component } from 'react';
-import {View, Text} from 'react-native'
+import {View, Text, Pressable, StyleSheet} from 'react-native'
 
 class CoinsScreen extends Component {
+    handlePress = () => {
+        this.props.navigation.navigate('CoinDetail')
+        console.log("Click")
+    }
+
     render(){
         return(
-            <View>
-                <Text>
+            <View style={styles.container}>
+                <Text style={styles.tittleText}>
                     Coin Screen
                 </Text>
+                <Pressable style={styles.btn} onPress={this.handlePress}>
+                    <Text style={styles.btnText}>Detail</Text>
+                </Pressable>
             </View >
         )
     }
 }
 export default CoinsScreen
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:"#333"
+    },
+    tittleText:{
+        color:"#fff",
+        textAlign:"center"
+    },
+    btn:{
+        padding:8,
+        backgroundColor:"#0000aa",
+        borderRadius:8,
+        margin:16,
+    },
+    btnText:{
+        color:"#fff",
+        textAlign:"center"
+    }
+})
